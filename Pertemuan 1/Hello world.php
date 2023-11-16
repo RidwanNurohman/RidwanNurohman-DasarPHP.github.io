784f80a52dc2ea3146e4db3673176072baf9f35d
RidwanNurohman <ridwanurohman2003@gmail.com>
2023-11-16 17:30:33 +0700

    first commit

---
+++ Pertemuan 1/Hello world.php
@@ -0,0 +1,22 @@
+<?php
+    $solo= ' Wah Itu Mobil Bagus Banget';
+    $tbk = ' Iya tuh Jadi pingin Punya yang kayak seperti itu';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h1><center>
+      Awas Mobil Ferrari Mau Lewat !!!!
+    </center></h1>
+    <?php
+    echo "<b><center>$solo</center></b>";
+    echo "<b><center>$tbk</center></b>";
+    ?>
+</body>
+</html>
